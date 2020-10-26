f1cbf8966c9036de45e4a368c1d8046b6a1d2c0a
Shawn <>
2020-10-26 20:35:41 +0800

    modify

--- leetcode/editor/cn/[20]有效的括号.java
+++ leetcode/editor/cn/[20]有效的括号.java
@@ -44,7 +44,30 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isValid(String s) {
+        // 判断字符的个数是否是偶数，如果奇数肯定不是有效括号
+        if(s.length()%2 !=0){
+            return false;
+        }
 
+        HashMap<Character, Character> map = new HashMap<Character, Character>(){{
+            put(')','(');
+            put(']','[');
+            put('}','{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+
+        for (int i = 0; i <s.length() ; i++) {
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                if(stack.isEmpty() || stack.peek() != map.get(ch)){
+                    return false;
+                }
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
